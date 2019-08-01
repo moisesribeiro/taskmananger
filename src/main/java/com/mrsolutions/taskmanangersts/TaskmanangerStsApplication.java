fa28f21e51d14ba95e5cb09f122faf3dbc5be8b4
moisesribeiro <moises.rsjr@gmail.com>
2019-08-01 17:10:21 -0300

    Initial Project Taskmananger

---
+++ src/main/java/com/mrsolutions/taskmanangersts/TaskmanangerStsApplication.java
@@ -0,0 +1,13 @@
+package com.mrsolutions.taskmanangersts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanangerStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanangerStsApplication.class, args);
+	}
+
+}
