dfb32759791ac6bd0fdb18afbff882ffbd8809bb
moisesribeiro <moises.rsjr@gmail.com>
2019-09-03 16:56:56 -0300

    commit create crud

--- src/main/java/com/mrsolutions/taskmanangersts/services/UserService.java
+++ src/main/java/com/mrsolutions/taskmanangersts/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mrsolutions.taskmanangersts.domain.User;
+import com.mrsolutions.taskmanangersts.dto.UserDTO;
 import com.mrsolutions.taskmanangersts.repository.UserRepository;
 
 @Service
@@ -24,5 +25,29 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(null);
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
